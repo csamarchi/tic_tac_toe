112ac064b8a6462c002b1a2e41f37bc93b8cf19e
Christine Samarchi <csamarchi@gmail.com>
2019-06-19 18:14:03 -0500

    able to track x wins and o wins

--- app.js
+++ app.js
@@ -21,24 +21,20 @@ const getCol = (className) => {
 const handleClick = (e) => {
   let row = getRow(e.target.className);
   let col = getCol(e.target.className);
-  let newState = !board[row][col]
-  board[row][col] = newState
-  if (newState) {
-    if (turnCounter % 2 === 0) {
-      e.target.style.background = 'blue'
-      e.target.innerHTML = 'X';
-      playerX.push(event.target.id)
-    } else {
-      e.target.style.background= 'red';
-      e.target.innerHTML = 'O';
-      playerO.push(event.target.id)
-    }
+  if (turnCounter % 2 === 0) {
+    e.target.style.background = 'blue'
+    e.target.innerHTML = 'X';
+    playerX.push(event.target.id)
+    board[row][col] = 'X';
   } else {
-    $(e.target).removeClass('clicked')
+    e.target.style.background= 'red';
+    e.target.innerHTML = 'O';
+    playerO.push(event.target.id)
+    board[row][col] = 'O';
   }
   turnCounter++
-  console.log(playerX, playerO)
-  checkForSolution();
+  checkForSolution('X');
+  checkForSolution('O');
 }
 
 const landing = () => {
@@ -49,30 +45,30 @@ const landing = () => {
 // Logic Layer
 
 const board = [
-  [false, false, false],
-  [false, false, false],
-  [false, false, false],
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
 ]
 
-const checkForSolution = () => {
-  let rows = checkRows();
-  let cols = checkCols();
-  let diag1 = checkDiag1();
-  let diag2 = checkDiag2();
+const checkForSolution = (mark) => {
+  let rows = checkRows(mark);
+  let cols = checkCols(mark);
+  let diag1 = checkDiag1(mark);
+  let diag2 = checkDiag2(mark);
 
   if (rows || cols || diag1 || diag2) {
-    console.log('WINNER');
+    console.log('WINNER!');
     return true
   } else {
     return false
   }
 }
 
-const checkRows = () => {
+const checkRows = (mark) => {
   for (let i = 0; i < board.length; i++) {
     let solved = true;
     for (let j = 0; j < board[i].length; j++) {
-      if (!board[i][j]) {
+      if (board[i][j] !== mark) {
         solved = false;
         break
       }
@@ -84,38 +80,36 @@ const checkRows = () => {
   return false
 }
 
-const checkCols = () => {
-for (let i = 0; i < board.length; i++) {
-  let solved = true;
-  for (let j = 0; j < board.length; j++) {
-    if (!board[j][i]) {
-      solved = false;
-      break
+const checkCols = (mark) => {
+  for (let i = 0; i < board.length; i++) {
+    let solved = true;
+    for (let j = 0; j < board.length; j++) {
+      if (board[j][i] !== mark) {
+        solved = false;
+        break
+      }
+    }
+    if (solved) {
+      return true
     }
   }
-  if (solved) {
-    return true
+  return false
+}
+
+const checkDiag1 = (mark) => {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i][i] !== mark) {
+      return false
+    }
   }
-}
-return false
-}
-
-const checkDiag1 = () => {
-
-  for (let i = 0; i < board.length; i++) {
-    if (!board[i][i]) {
-      return false
-    }
-  }
-  return true;
+  return true
 }
 
-const checkDiag2 = () => {
-
+const checkDiag2 = (mark) => {
   let colIndex = board.length - 1;
 
   for (let i = 0; i < board.length; i++) {
-    if (!board[i][colIndex]) {
+    if (board[i][colIndex] !== mark) {
       return false
     }
     colIndex--
